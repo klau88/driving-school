bdc068c73c1f48f9f4fe5a99a88affa7e90dc64b
klau88 <=>
2025-05-10 23:11:24 +0200

    Add initial admin user credentials for initial seeder

--- config/auth.php
+++ config/auth.php
@@ -112,4 +112,8 @@ return [
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
+    'admin' => [
+        'user' => env('ADMIN_USER'),
+        'password' => env('ADMIN_PASSWORD')
+    ]
 ];
